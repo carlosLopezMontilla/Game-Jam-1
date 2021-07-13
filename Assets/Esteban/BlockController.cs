61c36542ecf9dd07e68439cc22a1ed87310742e3
Esteban Donaire Martínez <edonmar@gmail.com>
2021-07-13 19:58:48 +0200

    Añadido movimiento constante de los bloques

--- Assets/Esteban/BlockController.cs
+++ Assets/Esteban/BlockController.cs
@@ -12,26 +12,28 @@ public class BlockController : MonoBehaviour
 
     private void MovementInput()
     {
-        float x = 0;
-        float y = 0;
+        float horMovement = 0;
+        float verMovement = 0;
 
-        if (Input.GetKeyDown(KeyCode.D))
-            x = -1;
-        else if (Input.GetKeyDown(KeyCode.A))
-            x = 1;
+        if (Input.GetKey(KeyCode.D))
+            horMovement = -1;
+        else if (Input.GetKey(KeyCode.A))
+            horMovement = 1;
 
-        if (Input.GetKeyDown(KeyCode.W))
-            y = 1;
-        else if (Input.GetKeyDown(KeyCode.S))
-            y = -1;
+        if (Input.GetKey(KeyCode.W))
+            verMovement = 1;
+        else if (Input.GetKey(KeyCode.S))
+            verMovement = -1;
 
-        if (x != 0 || y != 0)
-            MoveIn(x, y);
+        if (horMovement != 0 || verMovement != 0)
+            Move(horMovement, verMovement);
     }
 
-    private void MoveIn(float x, float y)
+    private void Move(float horMovement, float verMovement)
     {
-        transform.position += new Vector3(x, y, 0);
+        float movSpeed = 10;
+        transform.position += new Vector3(horMovement * movSpeed * Time.deltaTime,
+            verMovement * movSpeed * Time.deltaTime, 0);
     }
 
     private void RotationInput()
